df7ed77788d010c5f5129a50b45f85c46215ee1a
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-05-16 17:04:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('Allison Has a Fun Streamlit App!')
 streamlit.header('Welcome to the Dublin Office!)
 streamlit.text('We are so glad that you joined Snowflake. Allison has curated this list of local cafes, lunch spots, and hiking trails for your leisure.')
-                
+               
